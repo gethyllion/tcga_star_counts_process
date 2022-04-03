08069735c379d435c0f56176110cca590959472c
gethyllion <97503450+gethyllion@users.noreply.github.com>
2022-04-03 16:56:03 +0800

    Update tar2.py

--- tar2.py
+++ tar2.py
@@ -1,23 +1,41 @@
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 30 22:44:46 2022
 
+
+
+description ="""
+'this script is for establish sequence matrix from .gz meta_json of TCGA data STAR-counts, 
+you may assign specific value on [biotype] to extract mRNA or lncRNA ,eg biotype = protein_coding, that would make mRNAmatrix'
+#注意事项 1.压缩文件应该是.gz 结尾的文件 2.确保当前环境安装了 pandas和tqdm
 @author: ApriPro zhuj ex hangzhou 
 """
+biotype_types ="""
+'IG_V_pseudogene', 'IG_J_pseudogene', 
+'transcribed_unitary_pseudogene', 'scRNA', 'IG_J_gene', 
+'TR_J_pseudogene', 'vault_RNA', 'TEC', 'snRNA', 'ribozyme', 
+'scaRNA', 'translated_unprocessed_pseudogene', 'TR_V_pseudogene',
+ 'IG_V_gene', 'IG_pseudogene', 'Mt_tRNA', 'TR_D_gene', 'sRNA', 
+ 'IG_C_gene', 'misc_RNA', 'Mt_rRNA', 'snoRNA',
+ 'transcribed_processed_pseudogene', 
+'translated_processed_pseudogene', 'unprocessed_pseudogene', 'IG_C_pseudogene', 
+'pseudogene', 'IG_D_gene', 'rRNA', 'processed_pseudogene', 'TR_V_gene',
+ 'miRNA', 'polymorphic_pseudogene', 'transcribed_unprocessed_pseudogene', 
+ 'unitary_pseudogene', 'protein_coding', 'lncRNA', 'TR_J_gene', 'rRNA_pseudogene', 'TR_C_gene
+ '"""
 
 
 
-import os,sys, time
+from tqdm import tqdm as tq
+import os, time
 import pandas as pd
 import argparse as agp
 
-parser = agp.ArgumentParser(description='this script is for establish matrix from .gz or .zip and meta_json of TCGA data STAR-counts, you may assign specific value on [biotype] to extract mRNA or lncRNA ,eg biotype = protein_coding, that would make mRNAmatrix')
 
+parser = agp.ArgumentParser(description = description)
 parser.add_argument('-j', '--json_path', type = str, help = 'meta.json file path, rudimentary.')
-parser.add_argument('-t', '--tar_path', type = str, help = '.gz or .zip path of counts file, rudimentary')
-parser.add_argument('-b', '--biotype', type = str, help='biotype, protein_coding as default', default='protein_coding')
+parser.add_argument('-t', '--tar_path', type = str, help = 'path of counts file, suffix .gz, rudimentary')
+parser.add_argument('-b', '--biotype', type = str, help = f'sequence biotype, protein_coding(mRNA) as default, you may choose one from the following list(your input data could contains more types that are not indicated in the list):  {biotype_types}', default='protein_coding')
 
 cmd_argus = parser.parse_args()
 
@@ -76,10 +94,11 @@ def parser_tsv(tsv):
     return df.iloc[:,1:4]
 
 
-
+all_types = []
 def choose_biotype_make_dict(df, biotype = 'protein_coding'):
 
     biotype = biotype
+    all_types.extend(list(df.biotype.unique()))
     df = df[df['biotype'] == biotype]
     assert len(df.index) != 0
 
@@ -96,6 +115,7 @@ def parser_json_for_sample_id(json_path = None):
 
     file_name_df = pd.json_normalize(data= json_dict,)
     file_name, entity_submitter_id = file_name_df.file_name, id_df.entity_submitter_id
+
     
     return dict(zip(file_name,entity_submitter_id))
 
@@ -103,8 +123,9 @@ def parser_json_for_sample_id(json_path = None):
 @sep
 def normal_sample_detector(df=None, columns=None, stat=True):
     from collections import Counter
-    
-    columns = list(df.columns)
+    columns = df.columns
+
+    columns = list(columns)
 
     test_v = list()
     for i in columns:
@@ -123,7 +144,8 @@ def normal_sample_detector(df=None, columns=None, stat=True):
     if all(test_v):
         print('ALL NORMAL WARNINGS')
     if stat:
-        print(Counter(test_v))
+        print( Counter(test_v))
+        #要存的话 加下面这句
         pd.DataFrame([Counter(test_v)]).to_csv('report.csv')
         
     return test_v
@@ -142,7 +164,6 @@ if __name__ =='__main__':
 
     biotype = cmd_argus.biotype
 
-
     json_path = cmd_argus.json_path
 
     tar_path = cmd_argus.tar_path
@@ -152,7 +173,6 @@ if __name__ =='__main__':
     print(f'unzppied in to {dir}')
    
     res = get_the_tsv(dir)
-    from tqdm import tqdm as tq
     def tsvpools():
         for i in tq(res):
             v = parser_tsv(i)
@@ -170,3 +190,4 @@ if __name__ =='__main__':
     data.index.name='id'
     # print(data)
     data.to_csv(f'{biotype}_mRNAmatrix.txt')
+    print(set(all_types))
